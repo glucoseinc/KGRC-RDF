946566ec161c5626305ec887e2bfe6de903f2913
Fumiya Kubota <fumiya.kubota@glucose.jp>
2023-01-28 23:18:11 +0900

    minorfix

--- ui/next.config.js
+++ ui/next.config.js
@@ -1,6 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const isProd = process.env.NODE_ENV === 'production';
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+module.exports = {
+  assetPrefix: isProd ? '/weekly-schedule' : '',
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.fallback = { net: false, fs: false, child_process: false }
+    return config
+  }
+}
