6620b8bdb708a8974b74de7ab75de2075c2ab922
Fumiya Kubota <fumiya.kubota@glucose.jp>
2023-01-28 23:28:02 +0900

    fix for gh-pages

--- ui/next.config.js
+++ ui/next.config.js
@@ -1,8 +1,8 @@
-const isProd = process.env.NODE_ENV === 'production';
+const isProd = process.env.EXPORT === 'true';
 
 /** @type {import('next').NextConfig} */
 module.exports = {
-  assetPrefix: isProd ? '/weekly-schedule' : '',
+  assetPrefix: isProd ? '/risk-ui' : '',
   reactStrictMode: true,
   webpack: (config) => {
     config.resolve.fallback = { net: false, fs: false, child_process: false }
