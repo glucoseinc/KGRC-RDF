2435e7105c0d8d52cbe11d223879017b159adaa5
sasaujp <sasaujp@users.noreply.github.com>
2023-01-31 13:28:45 +0000

    deploy: 9c1b36405b9780269481950289393703cf3c7d6d

---
+++ _next/static/xQL9LwM6YWVScKgARW1q9/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/376-b29e4f5b2757428b.js","static/chunks/pages/index-1dc38f99fadab2a0.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
